be9660a7ed3d7bb160253b929e44ad0caaa6ab57
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-01-18 14:36:59 +0100

    Added tests for partials
    This includes one test which checks for future support of partials in
    other template directories.

--- spec/mustache_railstache_spec.rb
+++ spec/mustache_railstache_spec.rb
@@ -1,4 +1,7 @@
 require_relative '../lib/mustache_railstache.rb'
+require 'fakefs/safe'
+
+module Rails; end
 
 class EmptyStache < Mustache::Railstache
 end
@@ -41,6 +44,7 @@ describe Mustache::Railstache do
       MultipleExposedFieldsHash.fields_for_hash =~ [:bar, :lava]
     end
   end
+
   describe "#to_hash" do
     it "should return an empty hash for an empty class" do
       e = EmptyStache.new
@@ -70,6 +74,60 @@ describe Mustache::Railstache do
       m[:lava] = 'lamp'
       m.to_hash.should == {bar: 'man', lava: 'lamp'}
     end
+  end
 
+  describe "#partials" do
+    before(:each) do
+      FakeFS.activate!
+
+      @f = FullStache.new
+
+      @template_extension = "html.mustache"
+
+      rails_root = "/tmp/this_doesnt_exist"
+      @template_root = "#{rails_root}/app/templates"
+      @template_path = "#{@template_root}/sample"
+      @shared_path = "#{@template_root}/shared"
+      @other_path = "#{@template_root}/other_dir"
+
+      FileUtils.mkdir_p "#{@template_root}"
+      FileUtils.mkdir_p "#{@template_path}"
+      FileUtils.mkdir_p "#{@shared_path}"
+      FileUtils.mkdir_p "#{@other_path}"
+
+      Rails.stub(:root).and_return(Pathname.new(rails_root))
+
+      @f.stub(:template_file).and_return("#{@template_path}/test.html.mustache")
+    end
+
+    after(:each) do
+      FakeFS.deactivate!
+    end
+
+    it "should try to find the parial in template dir" do
+      @file = "base_template"
+      @filename = "_#{@file}.#{@template_extension}"
+
+      File.open("#{@template_path}/#{@filename}", 'w') { |f| f.write('template_dir') }
+
+      @f.partial(@file).should == "template_dir"
+    end
+
+    it "should try to find partials in shared dir" do
+      @file = "shared_template"
+      @filename = "_#{@file}.#{@template_extension}"
+
+      File.open("#{@shared_path}/#{@filename}", 'w') { |f| f.write('shared_dir') }
+
+      @f.partial(@file).should == "shared_dir"
+    end
+
+    it "should try to find the partial in other directories if a \/ is  detected" do
+      @filename = "other_dir/_other_template.#{@template_extension}"
+
+      File.open("#{@template_root}/#{@filename}", 'w') { |f| f.write('other_dir') }
+
+      @f.partial(@file).should == "other_dir"
+    end
   end
 end
